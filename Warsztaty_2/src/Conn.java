bc8d1768d655c62f87faeaf2c669dad9b6f1b7f3
MarcinK88 <marcin3216@gmail.com>
2019-03-10 14:54:56 +0100

    stworzona klasa User

--- Warsztaty_2/src/Conn.java
+++ Warsztaty_2/src/Conn.java
@@ -1,2 +1,20 @@
+import com.mysql.jdbc.Connection;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Conn {
+
+    public static Connection getConnection() throws SQLException {
+
+        Connection  connection = (Connection) DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/programming_school?useSSL=false",
+                    "root",
+                    "coderslab");
+
+        return connection;
+    }
+
 }
--- Warsztaty_2/src/Main.java
+++ Warsztaty_2/src/Main.java
@@ -1,6 +1,35 @@
+import com.mysql.jdbc.Connection;
+
+        import java.sql.PreparedStatement;
+        import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        try (Connection connection = Conn.getConnection()) {
+
+           User user1 = new User("name2", "mail2", "password");
+            user1.saveToDB(connection);
+
+//            User user2 = User.loadUserById(connection,1);
+//            System.out.println("imie: " + user2.getUsername() + " id: " + user2.getId());
+
+            ArrayList<User> users = User.loadAllUsers(connection);
+           for (User user : users) {
+                System.out.println("id: " + user.getId() + " imie: " + user.getUsername() + " email: " + user.getEmail());
+           }
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
 }
