15e01bf54427e3b64b7e021dfc78c9cf28aa1c26
Marcin Kucharczyk <marcin3216@gmail.com>
2019-03-12 11:27:18 +0100

    dodano klasę Group

--- Warsztaty_2/src/Group.java
+++ Warsztaty_2/src/Group.java
@@ -1,2 +1,89 @@
+import com.mysql.jdbc.Connection;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class Group {
+
+    private int id;
+    private String name;
+
+    public Group(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Group() {
+    }
+
+    public void saveToDB(Connection conn) throws SQLException {
+        if (this.id == 0) {
+            String sql = "INSERT INTO user_group(name) VALUES (?)";
+            PreparedStatement preparedStatement
+                    = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, this.name);
+            preparedStatement.executeUpdate();
+            ResultSet rs = preparedStatement.getGeneratedKeys();
+            if (rs.next()) {
+                this.id = rs.getInt(1);
+            }
+        } else {
+            String sql = "UPDATE user_group SET name=? where id = ?";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, this.name);
+            preparedStatement.setInt(2, this.id);
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    static public Group loadGroupById(Connection conn, int id) throws SQLException {
+        String sql = "SELECT * FROM user_group where id=?";
+        PreparedStatement preparedStatement = conn.prepareStatement(sql);
+        preparedStatement.setInt(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()) {
+            Group loadedGroup = new Group();
+            loadedGroup.id = resultSet.getInt("id");
+            loadedGroup.name = resultSet.getString("name");
+
+            return loadedGroup;}
+        return null;
+    }
+
+    static public ArrayList<Group> loadAllGroups(Connection conn) throws SQLException {
+        ArrayList<Group> groups = new ArrayList<Group>();
+        String sql = "SELECT * FROM user_group";
+        PreparedStatement preparedStatement = conn.prepareStatement(sql);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            Group loadedGroup = new Group();
+            loadedGroup.id = resultSet.getInt("id");
+            loadedGroup.name = resultSet.getString("name");
+            groups.add(loadedGroup);}
+
+        return groups;}
+
+    public void delete(Connection conn) throws SQLException {
+        if (this.id != 0) {
+            String sql = "DELETE FROM groups WHERE id=?";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setInt(1, this.id);
+            preparedStatement.executeUpdate();
+            this.id = 0;
+        }
+    }
+
 }
--- Warsztaty_2/src/Main5.java
+++ Warsztaty_2/src/Main5.java
@@ -1,6 +1,7 @@
 import java.sql.SQLException;
+import java.util.ArrayList;
 
-public class Main {
+public class Main5 {
     public static void main(String[] args) throws SQLException {
         DBConnection connection = null;
 
@@ -8,7 +9,10 @@ public class Main {
             connection = new DBConnection("programming_school", "root", "coderslab");
 
 
-
+            ArrayList<Group> groups = Group.loadAllGroups(connection.getConnection());
+            for (Group group : groups) {
+                System.out.println("id: " + group.getId() + " name:  " + group.getName());
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
